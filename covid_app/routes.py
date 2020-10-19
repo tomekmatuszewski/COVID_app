02ff391b31cab3aa6a9603497a33e2290d3fb673
TomekM <tomasz_matuszewski@o2.pl>
2020-10-19 16:45:56 +0200

    changes in summary

--- covid_app/routes.py
+++ covid_app/routes.py
@@ -1,24 +1,26 @@
 from flask import Flask, render_template
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 COVID_API_URL = "https://api.covid19api.com"
-
-ENDPOINTS = {
-    "summary": "/summary",
-}
 
 
 @app.route("/covid/summary")
 def get_all_countries():
-    summary = requests.get(COVID_API_URL+ENDPOINTS["summary"]).json()
-    return render_template("summary.html", summary=summary)
+    path = "/summary"
+    summary = requests.get(COVID_API_URL+path).json()
+    return render_template("summary.html", summary=summary, title="Countries")
 
 
 @app.route("/covid/<country_slug>")
-def country_confirmed_cases(country_slug):
+def country_day_one_confirmed_cases(country_slug):
     path = f"/dayone/country/{country_slug}/status/confirmed"
     cases_day = requests.get(COVID_API_URL+path).json()
-    return render_template("country_details_info.html", cases_day=cases_day)
+    return render_template("country_day_one_confirmed_cases.html", cases_day=cases_day)
+
+
+@app.template_filter()
+def numberFormat(value):
+    return format(int(value), ",d")
 

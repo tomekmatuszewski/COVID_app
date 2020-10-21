5abb29ce7e45beeadfb9ca6a1c691e19bc4d0f1b
TomekM <tomasz_matuszewski@o2.pl>
2020-10-21 14:22:44 +0200

    final

--- covid_app/tests/test_routes.py
+++ covid_app/tests/test_routes.py
@@ -1,5 +1,6 @@
-from covid_app.routes import app
 import pytest
+
+from covid_app.routes import app
 
 
 @pytest.fixture(name="client")
@@ -16,11 +17,11 @@ def test_get_all_countries_summary(client):
 @pytest.mark.parametrize(
     "country, exp_result",
     (
-            ("italy", 200),
-            ("grenada", 200),
-            ("poland", 200),
-            ("%%%%", 500),
-    )
+        ("italy", 200),
+        ("grenada", 200),
+        ("poland", 200),
+        ("%%%%", 500),
+    ),
 )
 def test_country_day_one_cases(client, country, exp_result):
     response = client.get(f"/covid/{country}")
@@ -30,12 +31,14 @@ def test_country_day_one_cases(client, country, exp_result):
 @pytest.mark.parametrize(
     "country,start_date,end_date,exp_result",
     (
-            ("spain", "2020-09-09", "2020-10-11",  200),
-            ("poland", "2020-03-09", "2020-10-11",  200),
-            ("%%%%", "2020-10-09", "2020-10-11",  500),
-            ("canada", "2020-10-09", "ppp",  500),
-    )
+        ("spain", "2020-09-09", "2020-10-11", 200),
+        ("poland", "2020-03-09", "2020-10-11", 200),
+        ("%%%%", "2020-10-09", "2020-10-11", 500),
+        ("canada", "2020-10-09", "ppp", 500),
+    ),
 )
-def test_country_start_end_date_cases(client, country, start_date, end_date, exp_result):
+def test_country_start_end_date_cases(
+    client, country, start_date, end_date, exp_result
+):
     response = client.get(f"/covid/{country}/{start_date}/{end_date}")
     assert response.status_code == exp_result

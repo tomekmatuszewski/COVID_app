207ff08a4197a26634e30793e009898235457a03
TomekM <tomasz_matuszewski@o2.pl>
2020-10-21 14:22:13 +0200

    finalizing

--- covid_app/routes.py
+++ covid_app/routes.py
@@ -1,55 +1,81 @@
-from flask import Flask, render_template, request
 import requests
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder="static")
 
 COVID_API_URL = "https://api.covid19api.com"
 
 ENDPOINTS = {
-    'summary': '/summary',
-    'confirmed_cases': "/dayone/country/{}/status/confirmed",
-    'deaths': "/dayone/country/{}/status/deaths",
-    'recovered': "/dayone/country/{}/status/recovered",
-    'cases_start_end': "/country/{}/status/confirmed?from={}T00:00:00Z&to={}T00:00:00Z",
-    'recovered_start_end': "/country/{}/status/recovered?from={}T00:00:00Z&to={}T00:00:00Z",
-    'deaths_start_end': "/country/{}/status/deaths?from={}T00:00:00Z&to={}T00:00:00Z",
+    "summary": "/summary",
+    "confirmed_cases": "/dayone/country/{}/status/confirmed",
+    "deaths": "/dayone/country/{}/status/deaths",
+    "recovered": "/dayone/country/{}/status/recovered",
+    "cases_start_end": "/country/{}/status/confirmed?from={}T00:00:00Z&to={}T00:00:00Z",
+    "recovered_start_end": "/country/{}/status/recovered?from={}T00:00:00Z&to={}T00:00:00Z",
+    "deaths_start_end": "/country/{}/status/deaths?from={}T00:00:00Z&to={}T00:00:00Z",
 }
 
 
 @app.route("/covid/summary")
 def get_all_countries():
-    summary = requests.get(COVID_API_URL+ENDPOINTS['summary']).json()
+    summary = requests.get(COVID_API_URL + ENDPOINTS["summary"]).json()
     return render_template("summary.html", summary=summary, title="Countries")
 
 
 @app.route("/covid/<country_slug>")
 def country_day_one_cases(country_slug):
-    cases_day = requests.get(COVID_API_URL + ENDPOINTS['confirmed_cases'].format(country_slug)).json()
-    deaths = requests.get(COVID_API_URL + ENDPOINTS['deaths'].format(country_slug)).json()
-    recovered = requests.get(COVID_API_URL + ENDPOINTS['recovered'].format(country_slug)).json()
-    title = cases_day[0]['Country']
-    country_code = cases_day[0]['CountryCode']
-    summary = requests.get(COVID_API_URL + ENDPOINTS['summary']).json()
-    return render_template("country_day_one_confirmed_cases.html", cases_day=cases_day,
-                           title=title, code=country_code, deaths=deaths, recovered=recovered, slug=country_slug,
-                           countries=summary)
+    cases_day = requests.get(
+        COVID_API_URL + ENDPOINTS["confirmed_cases"].format(country_slug)
+    ).json()
+    deaths = requests.get(
+        COVID_API_URL + ENDPOINTS["deaths"].format(country_slug)
+    ).json()
+    recovered = requests.get(
+        COVID_API_URL + ENDPOINTS["recovered"].format(country_slug)
+    ).json()
+    title = cases_day[0]["Country"]
+    country_code = cases_day[0]["CountryCode"]
+    summary = requests.get(COVID_API_URL + ENDPOINTS["summary"]).json()
+    return render_template(
+        "country_day_one_confirmed_cases.html",
+        cases_day=cases_day,
+        title=title,
+        code=country_code,
+        deaths=deaths,
+        recovered=recovered,
+        slug=country_slug,
+        countries=summary,
+    )
 
 
 @app.route("/covid/<country_slug>/<start_date>/<end_date>")
 def country_start_end_date_cases(country_slug, start_date, end_date):
 
-    cases = requests.get(COVID_API_URL +
-                         ENDPOINTS['cases_start_end'].format(country_slug, start_date, end_date)).json()
-    recovered = requests.get(COVID_API_URL +
-                             ENDPOINTS['recovered_start_end'].format(country_slug, start_date, end_date)).json()
-    deaths = requests.get(COVID_API_URL +
-                          ENDPOINTS['deaths_start_end'].format(country_slug, start_date, end_date)).json()
+    cases = requests.get(
+        COVID_API_URL
+        + ENDPOINTS["cases_start_end"].format(country_slug, start_date, end_date)
+    ).json()
+    recovered = requests.get(
+        COVID_API_URL
+        + ENDPOINTS["recovered_start_end"].format(country_slug, start_date, end_date)
+    ).json()
+    deaths = requests.get(
+        COVID_API_URL
+        + ENDPOINTS["deaths_start_end"].format(country_slug, start_date, end_date)
+    ).json()
     country_code = cases[0]["CountryCode"]
-    return render_template("country_cases_start_end_date.html", cases=cases, country=country_slug, start_date=start_date,
-                           end_date=end_date, deaths=deaths, recovered=recovered, code=country_code)
+    return render_template(
+        "country_cases_start_end_date.html",
+        cases=cases,
+        country=country_slug,
+        start_date=start_date,
+        end_date=end_date,
+        deaths=deaths,
+        recovered=recovered,
+        code=country_code,
+    )
 
 
 @app.template_filter()
 def numberFormat(value):
     return format(int(value), ",d")
-
---
+++ covid_app/tests/test_routes.py
@@ -0,0 +1,41 @@
+from covid_app.routes import app
+import pytest
+
+
+@pytest.fixture(name="client")
+def create_client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_get_all_countries_summary(client):
+    response = client.get("/covid/summary")
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "country, exp_result",
+    (
+            ("italy", 200),
+            ("grenada", 200),
+            ("poland", 200),
+            ("%%%%", 500),
+    )
+)
+def test_country_day_one_cases(client, country, exp_result):
+    response = client.get(f"/covid/{country}")
+    assert response.status_code == exp_result
+
+
+@pytest.mark.parametrize(
+    "country,start_date,end_date,exp_result",
+    (
+            ("spain", "2020-09-09", "2020-10-11",  200),
+            ("poland", "2020-03-09", "2020-10-11",  200),
+            ("%%%%", "2020-10-09", "2020-10-11",  500),
+            ("canada", "2020-10-09", "ppp",  500),
+    )
+)
+def test_country_start_end_date_cases(client, country, start_date, end_date, exp_result):
+    response = client.get(f"/covid/{country}/{start_date}/{end_date}")
+    assert response.status_code == exp_result
